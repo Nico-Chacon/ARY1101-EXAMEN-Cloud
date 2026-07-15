c551eb2ecda3a3a1e74df949a1080ee48a4848e4
NICKO_LARZ <nicochacon531@gmail.com>
2026-07-15 21:47:48 +0000

    fix 2

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "chacon-automoviltech-tfstate"
-    key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true
-    encrypt        = true
+    bucket       = "chacon-automoviltech-tfstate"
+    key          = "dev/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
